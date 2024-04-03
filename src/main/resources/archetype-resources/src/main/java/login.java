a9de769be34fc0ebc12a9ed9fb6f92bf10396a27
Dileepmasaram <164160563+Dileepmasaram@users.noreply.github.com>
2024-04-03 15:52:26 +0530

    Update login.java

--- src/main/resources/archetype-resources/src/main/java/login.java
+++ src/main/resources/archetype-resources/src/main/java/login.java
@@ -1,1 +1,13 @@
+package $package;
 
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
